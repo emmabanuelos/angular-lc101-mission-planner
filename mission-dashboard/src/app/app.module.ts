c4f8759770654b4eae550e7281a5915d4d51a6c8
Emma Banuelos <emmabanuelos@icloud.com>
2019-10-21 11:48:27 -0500

    finished studio without bonus mission

--- mission-dashboard/src/app/app.module.ts
+++ mission-dashboard/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CrewComponent } from './crew/crew.component';
+import { EquipmentComponent } from './equipment/equipment.component';
+import { ExperiementsComponent } from './experiements/experiements.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CrewComponent,
+    EquipmentComponent,
+    ExperiementsComponent
   ],
   imports: [
     BrowserModule
