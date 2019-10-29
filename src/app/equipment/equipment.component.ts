58c9dd87b962bb34198efd2f40ddbfd9a41742bf
Emma Banuelos <emmabanuelos@icloud.com>
2019-10-28 20:42:41 -0500

    what i finished at class end time

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -21,11 +21,21 @@ export class EquipmentComponent implements OnInit {
    cargoMass: number = 0;
    maximumAllowedMass: number = 2000;
    maxItems: number = 10;
+   
 
    constructor() { }
 
    ngOnInit() { }
 
    // Code your addItem function here:
-   
+   addItem(equipment:object) {
+     this.cargoHold.push(equipment);
+     this.cargoMass += equipment["mass"];
+     if (this.cargoMass >= 1800) {
+     return true ;
+     }
+     
+     return false;
+
+   }
 }
